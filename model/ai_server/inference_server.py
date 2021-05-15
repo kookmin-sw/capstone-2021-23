4b5566da7a4921a65205ad232c75271c450cf15e
ha-mulan <ham5312@naver.com>
2021-05-15 21:28:04 +0900

    inference.py modify

--- model/ai_server/inference_server.py
+++ model/ai_server/inference_server.py
@@ -1,11 +1,20 @@
 import os
 import argparse
 import os.path as osp
-
+import requests
 import torch
-
+from collections import defaultdict
 from mmaction.apis import inference_recognizer, init_recognizer
+import json
 
+server_url = "http://"+"192.168.0.15:8040"+"/users/email/"
+
+def StrConverter(filename:str)->dict:
+    key=['year','month','day','hour','min','sec']
+    filename=filename.replace(".",' ')
+    filename=filename.replace(":",' ')
+    data={ i:j for i,j in list(zip(key,filename.split())) }
+    return data
 receive_path= 'receive_video/'
 path_dir = 'video_for_process/'
 db       = "assult_candidate/"
@@ -32,7 +41,16 @@ while True:
             폭력 발생 db로 영상 보내야 함 and 처리 완료이므로 디렉토리에서 pop
             '''
             os.system(f"mv {path_dir+i} {db}")
+            data=StrConverter(i)
+            data["cam_num"]=0
+            print(data)
+            try:
+                r = requests.post(server_url, data= json.dumps(data))
+                print("post")
+            except:
+                print("error")
             print(f"moved to db and abnormal score : {results[0][1]}")
+            #웹서버로 보내버리기
 
         else:
             '''
