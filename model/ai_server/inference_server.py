b8365259b5bc73d66a1eb65625e8aee8696b1dfa
ha-mulan <ham5312@naver.com>
2021-05-19 17:44:55 +0900

    receive file

--- model/ai_server/inference_server.py
+++ model/ai_server/inference_server.py
@@ -10,11 +10,14 @@ import json
 server_url = "http://"+"192.168.0.15:8040"+"/users/email/"
 
 def StrConverter(filename:str)->dict:
+    # tm=time.gmtime(filename[:-4])
+    # avi_name=f"{tm.tm_year}.{tm.tm_mon}.{tm.tm_mday}.{tm.tm_hour}:{tm.tm_min}:{tm.tm_sec}"
     key=['year','month','day','hour','min','sec']
     filename=filename.replace(".",' ')
     filename=filename.replace(":",' ')
     data={ i:j for i,j in list(zip(key,filename.split())) }
     return data
+
 receive_path= 'receive_video/'
 path_dir = 'video_for_process/'
 db       = "assult_candidate/"
@@ -28,34 +31,38 @@ model = init_recognizer(
     device=device,
     )
 label="../demo/custom_map.txt"
+
 while True:
-    if not os.listdir(receive_path):
+    try:
+        if not os.listdir(receive_path):
+            continue
+        os.system(f"mv {receive_path}* {path_dir}")
+        file_list = os.listdir(path_dir)
+        file_list.sort()#시간순 정렬
+        for i in file_list:
+            results = inference_recognizer(model,path_dir+i,label)
+            if results[0][0]=="abnormal" and results[0][1]>0.86:
+                '''
+                폭력 발생 db로 영상 보내야 함 and 처리 완료이므로 디렉토리에서 pop
+                '''
+                os.system(f"mv {path_dir+i} {db}")
+                data=StrConverter(i)
+                data["cam_num"]=0
+                print(data)
+                try:
+                    r = requests.post(server_url, data= json.dumps(data))
+                    print("post")
+                except:
+                    print("server Receive error")
+                print(f"moved to db and abnormal score : {results[0][1]}")
+                #웹서버로 보내버리기
+
+            else:
+                '''
+                폭력 발생 x 이므로 디렉토리에서 영상 걍 삭제
+                '''
+                os.system(f"rm {path_dir+i}")
+                print(f"removed {path_dir+i} and :{results}")
+    except:
+        os.system(f"rm {path_dir}*")
         continue
-    os.system(f"mv {receive_path}* {path_dir}")
-    file_list = os.listdir(path_dir)
-    file_list.sort()#시간순 정렬
-    for i in file_list:
-        results = inference_recognizer(model,path_dir+i,label)
-        if results[0][0]=="abnormal" and results[0][1]>0.86:
-            '''
-            폭력 발생 db로 영상 보내야 함 and 처리 완료이므로 디렉토리에서 pop
-            '''
-            os.system(f"mv {path_dir+i} {db}")
-            data=StrConverter(i)
-            data["cam_num"]=0
-            print(data)
-            try:
-                r = requests.post(server_url, data= json.dumps(data))
-                print("post")
-            except:
-                print("error")
-            print(f"moved to db and abnormal score : {results[0][1]}")
-            #웹서버로 보내버리기
-
-        else:
-            '''
-            폭력 발생 x 이므로 디렉토리에서 영상 걍 삭제
-            '''
-            os.system(f"rm {path_dir+i}")
-            print(f"removed {path_dir+i} and :{results}")
-
