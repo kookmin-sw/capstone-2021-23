fdadd23bd4b91f897c6153c9c93d9c0eea61a137
pgh <pgh9801@gmail.com>
2021-05-20 15:37:12 +0000

    Cctv 테이블 생성

--- mysite/cctv/models.py
+++ mysite/cctv/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cctv(models.Model):
+    cam_id = models.AutoField(primary_key=True)
+    cam_location = models.CharField(max_length = 30)
+    cam_ip = models.CharField(max_length = 20, unique = True)
+    is_used = models.BooleanField(default=False)
