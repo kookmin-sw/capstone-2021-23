83c7e5a62a59185323cc7af40f377907014a9b56
pgh <pgh9801@gmail.com>
2021-05-20 15:13:12 +0000

    user_id 필드 max_lenght=30으로 수정

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-
+from cctv.models import Cctv 
+#import ..cctv.models import CCTV
 class Account(models.Model):
-    user_id = models.CharField(max_length= 20,unique=True)
+    # email lenngh > 20 인 경우도 있음
+    user_id = models.CharField(max_length= 30,unique=True)
     # auto_now_add : 최소 저장(insert)시에만 현재 날짜 저장
     sign_up_date = models.DateTimeField(auto_now_add=True)
-
+    cam_id = models.ForeignKey(Cctv, related_name = "cctv", on_delete = models.SET_NULL, null = True, db_column = "cam_id")
