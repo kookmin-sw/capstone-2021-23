2d85486c209586c4494d97839538c0074c08a8c0
pgh <pgh9801@gmail.com>
2021-05-20 15:16:19 +0000

    Cctv 테이블을 참조하기 위한 FK 설정

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -4,6 +4,7 @@ from cctv.models import Cctv
 class Account(models.Model):
     # email lenngh > 20 인 경우도 있음
     user_id = models.CharField(max_length= 30,unique=True)
-    # auto_now_add : 최소 저장(insert)시에만 현재 날짜 저장
+    # auto_now_add : 최초 저장(insert)시에만 현재 날짜 저장
     sign_up_date = models.DateTimeField(auto_now_add=True)
+    #Cctv 테이블을 참조
     cam_id = models.ForeignKey(Cctv, related_name = "cctv", on_delete = models.SET_NULL, null = True, db_column = "cam_id")
