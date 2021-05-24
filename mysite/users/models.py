58b5ac38deacafce0a5748cddc7d2b4cc740410f
pgh <pgh9801@gmail.com>
2021-05-24 15:27:52 +0000

    Cctv table 참조할 FK 설정

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from cctv.models import Cctv 
+#import ..cctv.models import CCTV
 class Account(models.Model):
-    user_id = models.CharField(max_length= 20,unique=True)
+    user_id = models.CharField(max_length= 30,unique=True)
     # auto_now_add : 최소 저장(insert)시에만 현재 날짜 저장
     sign_up_date = models.DateTimeField(auto_now_add=True)
-
+    cam_id = models.ForeignKey(Cctv, related_name = "cctv", on_delete = models.SET_NULL, null = True, db_column = "cam_id")
